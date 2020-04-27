d39bd1954799e87089e886b986b226c15fd98e42
powerpuff <powerpuff2k@gmail.com>
2020-04-27 20:53:44 +1200

    Changing typo Adding sub setup and teardown class to run steps before every test

--- Xero/TestProjectXero/Demo/Pages/BankAccountPage.py
+++ Xero/TestProjectXero/Demo/Pages/BankAccountPage.py
@@ -8,10 +8,11 @@ class Bank_Accounts(object):
             self.driver = driver
 
     def Org(self):
-        self.driver.find_element(By.CLASS_NAME, 'xrh-appmenucontainer').click()
+        self.driver.find_element(By.CLASS_NAME, Locators.Locators.Current_Org).click()
 
-    def Account(self, bank_name):
-        self.driver.find_element(By.XPATH, Locators.Locators.Bank).send_keys(bank_name)
+    def Find_your_bank(self, bank_name):
+        self.driver.find_element(By.XPATH, Locators.Locators.Bank_Name).send_keys(bank_name)
+
         # test.send_keys(Keys.TAB)
 
     def Result( self ):
@@ -22,15 +23,15 @@ class Bank_Accounts(object):
        self.driver.find_element(By.XPATH, Locators.Locators.AccountName).send_keys(acc_name)
 
     def Account_Type(self):
-        self.driver.find_element(By.CSS_SELECTOR, 'input[id^="accounttype"]').click()
-        self.driver.find_element(By.CSS_SELECTOR, "li.ba-combo-list-item").click()
+        self.driver.find_element(By.CSS_SELECTOR, Locators.Locators.Account_type_dropdown).click()
+        self.driver.find_element(By.CSS_SELECTOR,Locators.Locators.Account_type_selection).click()
 
 
     def Account_number(self, acc_number):
-        self.driver.find_element(By.ID, 'accountnumber-1068').send_keys(acc_number)
+        self.driver.find_element(By.XPATH, Locators.Locators.AccountNumber).send_keys(acc_number)
 
     def Continue(self):
-        self.driver.find_element(By.CSS_SELECTOR , 'a[data-automationid="continueButton"]').click()
+        self.driver.find_element(By.CSS_SELECTOR , Locators.Locators.Continue_button).click()
 
 
 
